f908051407d6362c09bf6d0f69a58f3d32df869c
nagysandordominik <nagysandordominik@icloud.com>
2021-08-28 16:20:15 +0200

    movie selection from dropdown menu

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ const fetchData = async (searchTerm) => {
     return response.data.Search;
 
 };
-
+// Search result display with image and its title 
 const root = document.querySelector('.autocomplete');
 root.innerHTML = `
 <label><b>Search for a movie</b></label>
 <input class="input" />
-<div class="dropdown is-active">
+<div class="dropdown">
       <div class="dropdown-menu">
         <div class="dropdown-content results"></div>
       </div>
@@ -34,6 +34,7 @@ const resultsWrapper = document.querySelector('.results');
 const onInput = async event => {
     const movies = await fetchData(event.target.value);
 
+    // When there is nothing in input, does not show the dropdown menu 
     if (!movies.length) {
         dropdown.classList.remove('is-active');
         return;
@@ -45,19 +46,30 @@ const onInput = async event => {
         const option = document.createElement('a');
         const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
 
+        // Displaying the movie poster and its title 
         option.classList.add('dropdown-item')
         option.innerHTML = `
             <img src="${imgSrc}" />
             ${movie.Title}
         `;
+        option.addEventListener('click', () => {
+            dropdown.classList.remove('is-active');
+            input.value = movie.Title;
+            onMovieSelect(movie);
+        });
 
         resultsWrapper.appendChild(option);
     }
 };
 input.addEventListener('input', debounce(onInput, 500));
 
+// Whenever clicking somewhere else than the dropdown menu, it disappears
 document.addEventListener('click', event => {
     if (!root.contains(event.target)) {
         dropdown.classList.remove('is-active')
     }
 });
+
+const onMovieSelect = movie => {
+    console.log(movie)
+};
