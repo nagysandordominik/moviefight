2470fd07a171b4a0aef75d5094b82468d5012f07
nagysandordominik <nagysandordominik@icloud.com>
2021-08-31 19:12:12 +0200

    Passing fetchData function as an argument

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-// Fetching to OMDbAPI
-const fetchData = async (searchTerm) => {
-    const response = await axios.get('http://www.omdbapi.com/', {
-        params: {
-            apikey: 'c2523724',
-            s: searchTerm
-        }
-    });
-    if (response.data.Error) {
-        return [];
-    }
-    return response.data.Search;
-
-};
-
 createAutoComplete({
     root: document.querySelector('.autocomplete'),
     renderOption(movie) {
@@ -27,6 +12,20 @@ createAutoComplete({
     },
     inputValue(movie) {
         return movie.Title;
+    },
+    async fetchData(searchTerm) {
+        const response = await axios.get('http://www.omdbapi.com/', {
+            params: {
+                apikey: 'c2523724',
+                s: searchTerm
+            }
+        });
+
+        if (response.data.Error) {
+            return [];
+        }
+
+        return response.data.Search;
     }
 });
 
