66d029eafec09ca56c77b7b5ca0bfbe7be3d6bdb
nagysandordominik <nagysandordominik@icloud.com>
2021-09-01 19:45:58 +0200

    API call fixed HTTP to HTTPS

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const autocompleteConfig = {
     },
 
     async fetchData(searchTerm) {
-        const response = await axios.get('http://www.omdbapi.com/', {
+        const response = await axios.get('https://www.omdbapi.com/', {
             params: {
                 apikey: 'c2523724',
                 s: searchTerm
